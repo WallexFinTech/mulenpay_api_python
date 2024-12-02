e7d34845a780211a859ecbeed9c96427bf6ba539
WallexFinTech <support@amlkyc.tech>
2024-12-02 21:05:43 +0500

    edited create payments rout

--- mulenpay_api/schemas.py
+++ mulenpay_api/schemas.py
@@ -1,5 +1,19 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    description: str
+    quantity: float
+    price: float
+    vat_code: int
+    payment_subject: int
+    payment_mode: int
+    product_code: str | None
+    country_of_origin_code: str | None
+    customs_declaration_number: str | None
+    excise: str | None
+    measurement_unit: int | None
 
 
 class CreatePayment(BaseModel):
@@ -10,4 +24,5 @@ class CreatePayment(BaseModel):
     description: str
     subscribe: Optional[str] = None
     holdTime: Optional[str] = None
+    items: List[Item]
 
